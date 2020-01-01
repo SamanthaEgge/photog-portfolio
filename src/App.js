4cb7ed3efcb12399dfb55294e01658ff948ca9b4
Samantha Egge <samantha.c.egge@gmail.com>
2019-12-31 17:35:26 -0800

    Initializing the project, adding sass.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './components/home/home.jsx'
+
 function App() {
   return (
     <div>
---
+++ src/components/home/home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const HomePage = () => (
+  <div className='home'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='menu-content'>
+          <h1 className='menu-title'>Headshots</h1>
+          <span className='menu-subtitle'>View</span>
+        </div>
+        <div className='menu-content'>
+          <h1 className='menu-title'>Landscapes</h1>
+          <span className='menu-subtitle'>View</span>
+        </div>
+        <div className='menu-content'>
+          <h1 className='menu-title'>Animal</h1>
+          <span className='menu-subtitle'>View</span>
+        </div>
+        <div className='menu-content'>
+          <h1 className='menu-title'>Prints</h1>
+          <span className='menu-subtitle'>Purchase</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage
