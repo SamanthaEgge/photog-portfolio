e9d9ace2abe9bf4b294cbf3f10f7f69cee69177d
Samantha Egge <samantha.c.egge@gmail.com>
2020-01-03 10:19:06 -0800

    Installed react-router-dom, started adding in routes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom'
 
+import './App.css';
 import HomePage from './pages/home/home.jsx'
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        {/* <Route exact path='/headshots' component={Headshots} />
+        <Route exact path='/wildlife' component={Wildlife} />
+        <Route exact path='/landscapes' component={Landscapes} />
+        <Route exact path='/prints' component={Prints} /> */}
+      </Switch>
+    </div>
   );
 }
 
