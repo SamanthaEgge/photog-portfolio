ac92a24f981e887a5f49a00a60016a6de1415efd
Samantha Egge <samantha.c.egge@gmail.com>
2020-03-09 15:52:13 -0700

    Created the initial user auth space, created sign in page and local state/handlers.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from './pages/home/home.jsx'
 import ShopPage from './pages/shop/shop.jsx'
 import Header from './components/header/header.jsx'
+import UserAuth from './pages/user-auth/user-auth.jsx'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='signin' component={UserAuth} />
         {/* <Route exact path='/headshots' component={Headshots} />
         <Route exact path='/wildlife' component={Wildlife} />
         <Route exact path='/landscapes' component={Landscapes} />
---
+++ src/pages/user-auth/user-auth.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './user-auth.scss'
+
+const UserAuth = () => (
+  <div className='user-auth'>SIGN IN</div>
+)
+
+export default UserAuth
