124686e1c3fcfe5ee49dea87a3ff98559fa69a39
Samantha Egge <samantha.c.egge@gmail.com>
2020-03-10 09:37:15 -0700

    Fixed sign in with google issue with the form. Users can now utilize google to sign in.

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -49,7 +49,7 @@ class SignIn extends React.Component {
             required />
 
           <CustomButton type='submit'>Sign In</CustomButton>
-          <CustomButton onSubmit={signInWithGoogle}>With Google</CustomButton>
+          <CustomButton onClick={signInWithGoogle}>Sign In With Google</CustomButton>
         </form>
       </div>
     )
