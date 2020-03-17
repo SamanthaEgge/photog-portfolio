922fe932beb0889f7c881e0158236acd212f0f7f
Samantha Egge <samantha.c.egge@gmail.com>
2020-03-17 12:24:08 -0700

    Added quantity count to shop items in redux store via cart utils file. Modified collection item and previews to allow adding items to count. Update dummy data.

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -6,24 +6,30 @@ import { addItem } from '../../redux/cart/cart-actions'
 
 import './collection-item.scss'
 
-const CollectionItem = ({ id, name, imageUrl, price, addItem}) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-      />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
-      <CustomButton inverted>Add to cart</CustomButton>
-  </div>
-)
+const CollectionItem = ({item, addItem}) => {
+  const { name, imageUrl, price } = item
+  console.log(addItem, 'addItem function')
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+        />
+        <div className='collection-footer'>
+          <span className='name'>{name}</span>
+          <span className='price'>{price}</span>
+        </div>
+        <CustomButton onClick={() => addItem(item)} inverted>
+          Add to cart
+        </CustomButton>
+    </div>
+  )  
+}
 
 const mapDispatchToProps = dispatch => ({
-  addItem: item => dispatch(addItem())
+  addItem: item => dispatch(addItem(item))
 })
 
 export default connect(null, mapDispatchToProps)(CollectionItem)
