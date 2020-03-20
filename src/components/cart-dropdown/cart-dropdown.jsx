5b50328f3208c8092a85543cbbce0bfa2c4ef4e0
Samantha Egge <samantha.c.egge@gmail.com>
2020-03-20 16:26:47 -0700

    Created checkout item component for displaying items within the checkout page.

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from 'reselect'
 import { withRouter } from 'react-router-dom'
 
 import { selectCartItems } from '../../redux/cart/cart-selector.js'
+import { toggleCartHidden } from '../../redux/cart/cart-actions'
 import CustomButton from '../custom-button/custom-button.jsx'
 import CartItem from '../cart-item/cart-item'
 
@@ -18,7 +19,11 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className='empty-message'>Your cart is empty</span>
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+    <CustomButton 
+      onClick={() => {
+        history.push('/checkout')
+        dispatch(toggleCartHidden())
+      }}>GO TO CHECKOUT</CustomButton>
   </div>
 )
 
